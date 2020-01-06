ff1e8f3f83a05ffe4b25be45947421b30e6f19e7
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-01-06 18:19:35 +0800

    Update SecurityLayout.tsx

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -36,11 +36,14 @@ class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayout
 
   render() {
     const {isReady} = this.state;
-    const {children, loading, currentUser} = this.props;
+    const {children, loading,
+      // currentUser
+    } = this.props;
     // console.log(currentUser, 'currentUser');
     // You can replace it to your authentication rule (such as check token exists)
     // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
-    const isLogin = currentUser && currentUser.userId;
+    // const isLogin = currentUser && currentUser.userId;
+    const isLogin = !!window.localStorage.getItem('authorization');
     const queryString = stringify({
       redirect: window.location.href,
     });
